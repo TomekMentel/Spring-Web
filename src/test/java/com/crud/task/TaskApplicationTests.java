cd2a7a03173d0e037300b03f3847fe8dbb01de4c
Tomasz Mentel <mentell@o2.pl>
2019-08-05 22:00:28 +0200

    modul 19.2.

--- src/test/java/com/crud/task/TaskApplicationTests.java
+++ src/test/java/com/crud/task/TaskApplicationTests.java
@@ -8,9 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TaskApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+	public void contextLoads() {	}
 }
