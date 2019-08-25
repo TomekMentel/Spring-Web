ab098d955240dff91afd58b8a88e2cd01f05b5bf
Tomasz Mentel <mentell@o2.pl>
2019-08-25 20:15:20 +0200

    modul 22.2 exercise

--- src/main/java/com/crud/tasks/TaskApplication.java
+++ src/main/java/com/crud/tasks/TaskApplication.java
@@ -11,9 +11,9 @@ public class TaskApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApplication.class, args);
 	}
-//	@Override
-  //  protected SpringApplicationBuilder configure (SpringApplicationBuilder aplication){
-	//    return aplication.sources(TaskApplication.class);
-    //}
+	@Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder aplication){
+	    return aplication.sources(TaskApplication.class);
+    }
 
 }
