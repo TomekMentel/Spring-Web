9eba7d996f06f1e436ae2f9d86ddecb093730597
Tomasz Mentel <mentell@o2.pl>
2019-08-26 19:12:39 +0200

    modul 22.2

--- src/main/java/com/crud/tasks/config/TrelloClient.java
+++ src/main/java/com/crud/tasks/config/TrelloClient.java
@@ -4,6 +4,7 @@ import com.crud.tasks.domain.TrelloBoardDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -11,6 +12,8 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static java.util.Optional.ofNullable;
 
 @Component
 public class TrelloClient {
@@ -24,21 +27,31 @@ public class TrelloClient {
     @Value("${trello.app.token}")
     private String trelloToken;
 
+    @Value("${trello.app.username}")
+    private String trelloUserName;
+
     @Autowired
     private RestTemplate restTemplate;
 
     public List<TrelloBoardDto> getTrelloBoards() {
-      //  TrelloBoardDto[] boardsResponse = restTemplate.getForObject(
-                URI url;
-        url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/tomaszmentel/boards")
+        URI url = getUri();
+
+        try {
+            TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
+            return Arrays.asList((ofNullable(boardsResponse).orElse(new TrelloBoardDto[0])));
+        } catch (RestClientException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    private URI getUri() {
+        URI url;
+        url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUserName + "/boards")
                 .queryParam("key", trelloAppKey)
                 .queryParam("token", trelloToken)
                 .queryParam("fields", "name,id").build().encode().toUri();
-
-        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
-  if (boardsResponse!=null){
-      return Arrays.asList(boardsResponse);
-  }
-        return new ArrayList<>();
+        return url;
     }
 }
+
+
